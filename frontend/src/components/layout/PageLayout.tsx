b0f30d877190dbf4fe34fb46f101a5b14b2366b4
moai-redcap <s.ono@jh-gk.com>
2025-05-07 23:20:03 +0900

    PC時チャットを右に固定（aboutページのみ）

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -2,20 +2,35 @@ import type { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { useLocation } from "react-router-dom";
 
 interface PageLayoutProps {
   children: ReactNode;
 }
 
 const PageLayout = ({ children }: PageLayoutProps) => {
-  return (
-    <div className="min-h-screen flex flex-col pt-14">
-      <Header />
-      <Sidebar />
-      <main className="flex-grow xl:ml-[260px] px-4 xl:px-8">{children}</main>
-      <Footer />
-    </div>
-  );
+  const nowPagePath = location.pathname;
+  if (nowPagePath === "/about") {
+    return (
+      <div className="min-h-screen flex flex-col pt-14">
+        <Header />
+        <Sidebar />
+        <main className="flex-grow xl:ml-[260px] px-4 xl:pl-8 xl:mr-[400px]">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div className="min-h-screen flex flex-col pt-14">
+        <Header />
+        <Sidebar />
+        <main className="flex-grow xl:ml-[260px] px-4 xl:px-8">{children}</main>
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default PageLayout;
